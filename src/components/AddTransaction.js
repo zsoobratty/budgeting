b72cbbf935105e496d0086e0a7d7c672e30f6c77
zsoobratty <49350914+zsoobratty@users.noreply.github.com>
2020-09-02 14:45:12 +0100

    Clear fields on submission

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -25,6 +25,11 @@ const AddTransaction = () => {
         }
 
         addIncome(newIncomeTransaction)
+
+        setIncome({
+            incomeText: '',
+            incomeAmount: 0
+        })
     }
 
     const [expense, setExpense] = useState({
@@ -48,21 +53,26 @@ const AddTransaction = () => {
         }
 
         addExpense(newExpenseTransaction)
+
+        setExpense({
+            expenseText: '',
+            expenseAmount: 0
+        })
     }
 
     return (
         <div className='form-wrapper'>
             <form onSubmit={onSubmitIncome}>
                 <div className="input-group income">
-                    <input type="text" name='incomeText' placeholder='Add Income...' autoComplete='off' onChange={onChangeIncome}/>
-                    <input type="number" name='incomeAmount' placeholder='Amount' autoComplete='off' onChange={onChangeIncome}/>
+                    <input type="text" name='incomeText' value={incomeText} placeholder='Add Income...' autoComplete='off' onChange={onChangeIncome}/>
+                    <input type="number" name='incomeAmount' value={incomeAmount} placeholder='Amount' autoComplete='off' onChange={onChangeIncome}/>
                     <input type="submit" value="Submit"/>
                 </div>
             </form>
             <form onSubmit={onSubmitExpense}>
                 <div className="input-group expense">
-                    <input type="text" name='expenseText' placeholder='Add Expense...' autoComplete='off' onChange={onChangeExpense}/>
-                    <input type="number" name='expenseAmount' placeholder='Amount' autoComplete='off' onChange={onChangeExpense}/>
+                    <input type="text" name='expenseText' value={expenseText} placeholder='Add Expense...' autoComplete='off' onChange={onChangeExpense}/>
+                    <input type="number" name='expenseAmount' value={expenseAmount} placeholder='Amount' autoComplete='off' onChange={onChangeExpense}/>
                     <input type="submit" value="Submit"/>
                 </div>
             </form>
