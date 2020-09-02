b7ed216372d5061840dc86b1a9b4b58373ec231a
zsoobratty <49350914+zsoobratty@users.noreply.github.com>
2020-09-02 14:09:09 +0100

    Separate list items into separate component

--- src/components/IncomeList.js
+++ src/components/IncomeList.js
@@ -1,5 +1,6 @@
 import React, {useContext} from 'react'
 import {GlobalContext} from '../context/GlobalState'
+import IncomeTransaction from './IncomeTransaction'
 
 const IncomeList = () => {
     const {incomeTransactions} = useContext(GlobalContext)
@@ -11,14 +12,7 @@ const IncomeList = () => {
             <h2>Transaction History</h2>
             <ul className="transaction-list">
                 {incomeTransactions.map(incomeTransaction => (
-                    <li className="transaction">
-                        <span className="transaction-text">{incomeTransaction.incomeText}</span>
-                        <span className="transaction-amount">£{incomeTransaction.incomeAmount}</span>
-                        <button className='delete-btn'>
-                            <i className="fas fa-trash">
-                            </i>
-                        </button>
-                    </li>
+                    <IncomeTransaction incomeTransaction={incomeTransaction}/>
                 ))}
             </ul>   
         </div>
---
+++ src/components/IncomeTransaction.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const IncomeTransaction = ({incomeTransaction}) => {
+    return (
+        <li className="transaction">
+            <span className="transaction-text">{incomeTransaction.incomeText}</span>
+            <span className="transaction-amount">£{incomeTransaction.incomeAmount}</span>
+            <button className='delete-btn'>
+                <i className="fas fa-trash">
+                </i>
+            </button>
+        </li>
+    )
+}
+
+export default IncomeTransaction
