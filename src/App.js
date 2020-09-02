c748a9f20fa500b1930537a155b8f202345aa8b6
zsoobratty <49350914+zsoobratty@users.noreply.github.com>
2020-09-02 13:48:04 +0100

    Add IncomeList componenet to App

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Header from './components/Header'
 import Balance from './components/Balance'
 import AddTransaction from './components/AddTransaction'
+import IncomeList from './components/IncomeList'    
 import './App.css'
 
 const App = () => {
@@ -11,6 +12,7 @@ const App = () => {
                 <Header />
                 <Balance />
                 <AddTransaction />
+                <IncomeList />
             </div>
         </div>
     )
