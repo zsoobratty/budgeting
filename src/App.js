1d899df222fb63e10f4c67153b3c0a85feb91871
zsoobratty <49350914+zsoobratty@users.noreply.github.com>
2020-09-02 13:44:51 +0100

    Add AddTransaction component to App

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Header from './components/Header'
 import Balance from './components/Balance'
+import AddTransaction from './components/AddTransaction'
 import './App.css'
 
 const App = () => {
@@ -9,6 +10,7 @@ const App = () => {
             <div className='app-wrapper'>
                 <Header />
                 <Balance />
+                <AddTransaction />
             </div>
         </div>
     )
