2cf83defd98ffef6aeba640668001de55a7ddd32
zsoobratty <49350914+zsoobratty@users.noreply.github.com>
2020-09-02 13:33:47 +0100

    Add CSS to App

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import './App.css'
 
 const App = () => {
     return (
-        <div>
-            <h1>Hello Budgeting</h1>
+        <div className='container'>
+            <div className='app-wrapper'>
+                <h1>Hello Budgeting</h1>
+            </div>
         </div>
     )
 }
